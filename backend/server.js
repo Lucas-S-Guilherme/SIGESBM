75ecd29e528a982f6079c7c2bd48a1181fec6545
Lucas Guilherme <lucassguilherme159@gmail.com>
2026-01-12 20:53:15 -0400

    arrumado maestro server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,15 @@
 const express = require('express');
-
 const cors = require('cors');
+const combatenteRoutes = require('./routes/combatentes'); // importa rotas
 
 const app = express();
 app.use(cors());
 app.use(express.json()); //Para o servidor entender JSON
 
-db.connect(err => {
-    if (err) {
-        console.error('Erro ao conectar ao banco:', err);
-        return;
-    }
-    console.log('Conectando ao banco SIGESC!');
-});
+//diz ao express para usar o arquivo combatente.js à toda rota /combatentes
 
-//Rota para listar combatentes
+app.use('/combatentes', combatenteRoutes);
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+})
